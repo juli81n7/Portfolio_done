1e09fc94e8493329111564bb0ef928c20019c557
“juli81n7” <j.elikofer@gmail.com>
2026-05-26 16:00:02 +0200

    Made some updates

--- src/components/ProjectSwiper.jsx
+++ src/components/ProjectSwiper.jsx
@@ -20,8 +20,8 @@ export default function ProjectSwiper({ data }) {
         simulateTouch="true"
       >
         {data.map((item, i) => (
-          <SwiperSlide className="rounded-xl" key={i}>
-            <div className="flex flex-col items-center justify-end h-full py-24 bg-black rounded-xl relative ">
+          <SwiperSlide className="rounded-lg" key={i}>
+            <div className="flex flex-col items-center justify-end h-full py-24 bg-black rounded-lg relative ">
               <h3 className="mt-4 text-lg font-semibold text-white  text-center z-10">
                 {item.project.name}
               </h3>
@@ -31,7 +31,7 @@ export default function ProjectSwiper({ data }) {
               <img
                 src={item.url}
                 alt={item.alt}
-                className="absolute w-full h-full object-cover rounded-xl top-0 left-0 z-0"
+                className="absolute w-full h-full object-cover rounded-lg top-0 left-0 z-0"
               />
             </div>
           </SwiperSlide>
